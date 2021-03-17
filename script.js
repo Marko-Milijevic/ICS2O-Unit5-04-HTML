0f63073bb7362aa214ee3ceaf778fd11a3ea2b14
MarkoMilijevic <Marko.Milijevic@ocsbstudent.ca>
2021-03-17 17:36:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById('change-image').addEventListener('click', image)
+document.getElementById('change-text').addEventListener('click', text)
+document.getElementById('hide').addEventListener('click', hide)
+document.getElementById('show').addEventListener('click', show)
+document.getElementById('bg').addEventListener('click', background)
+
+function image () {
+  document.getElementById('picture').src = './images/Ronaldo.jpg'
+}
+
+function text () {
+  document.getElementById('text').innerHTML = 'Lionel Messi and Cristiano Ronaldo'
+}
+
+function hide () {
+  document.getElementById('text').style.display = 'none'
+}
+
+function show () {
+  document.getElementById('text').style.display = 'block'
+}
+
+function background () {
+  document.body.style.backgroundColor = 'aqua'
+}
